44211f2f8d65e858a6cbc0f67498a71298c84d50
peterudu <puduevborho7@gmail.com>
2024-04-02 22:16:51 +0000

    Set new secret key

--- holidaysights/settings.py
+++ holidaysights/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l=rhztkq&q-q0r74*4wlf50jdaj4db)-k1aq+z_i0fmoa49io_'
+# SECRET_KEY = 'django-insecure-l=rhztkq&q-q0r74*4wlf50jdaj4db)-k1aq+z_i0fmoa49io_'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-peterudu-holidayblog-gambrlkdxqs.ws-eu110.gitpod.io','.herokuapp.com']
 
